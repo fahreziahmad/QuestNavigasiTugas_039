93d5aab5d03ad6d12382f117289fdc1c4e7a65e9
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-09 19:52:40 +0700

    Tambah data class FormData untuk menyimpan input pengguna

--- app/src/main/java/com/example/questnavigasitugas_039/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_039/Navigasi.kt
@@ -1,2 +1,24 @@
+// Nama File: Navigasi.kt
+// (Pastikan package Anda sesuai)
 package com.example.questnavigasitugas_039
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasitugas_039.view.DashboardScreen
+import com.example.questnavigasitugas_039.view.FormulirScreen
+import com.example.questnavigasitugas_039.view.TampilDataScreen
+
+// --- KITA DEFINISIKAN DATA CLASS DI SINI ---
+// (Agar bisa dipakai oleh TampilData.kt dan Formulir.kt)
+data class FormData(
+    val nama: String,
+    val gender: String,
+    val status: String,
+    val alamat: String
+)
+
+// --- DATA AWAL KITA PINDAHKAN KE SINI ---
